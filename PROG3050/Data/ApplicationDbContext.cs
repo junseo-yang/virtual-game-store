b00812f30bdc9340886d07eae792b98c2588026f
junseo-yang <jsy724724@gmail.com>
2023-10-04 14:03:28 -0400

    Fix: Create PROG3050 Project

---
+++ PROG3050/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PROG3050.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
