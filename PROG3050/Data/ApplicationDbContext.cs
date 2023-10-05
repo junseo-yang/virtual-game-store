d97c8b6d96a902e044d88cebb2d742c7dbe8319d
junseo-yang <jsy724724@gmail.com>
2023-10-05 10:19:42 -0400

    Fix: Create Country Schema

--- PROG3050/Data/ApplicationDbContext.cs
+++ PROG3050/Data/ApplicationDbContext.cs
@@ -51,6 +51,88 @@ namespace PROG3050.Data
                     CountryName = "Canada"
                 }
             );
+
+
+            builder.Entity<Province>().HasData(
+                new Province
+                {
+                    ProvinceId = 1,
+                    ProvinceName = "AB",
+                    CountryId = 1
+                },
+                new Province
+                {
+                    ProvinceId = 2,
+                    ProvinceName = "BC",
+                    CountryId = 1
+                },
+                new Province
+                {
+                    ProvinceId = 3,
+                    ProvinceName = "MB",
+                    CountryId = 1
+                },
+                new Province
+                {
+                    ProvinceId = 4,
+                    ProvinceName = "NB",
+                    CountryId = 1
+                },
+                new Province
+                {
+                    ProvinceId = 5,
+                    ProvinceName = "NL",
+                    CountryId = 1
+                },
+                new Province
+                {
+                    ProvinceId = 6,
+                    ProvinceName = "NT",
+                    CountryId = 1
+                },
+                new Province
+                {
+                    ProvinceId = 7,
+                    ProvinceName = "NS",
+                    CountryId = 1
+                },
+                new Province
+                {
+                    ProvinceId = 8,
+                    ProvinceName = "NU",
+                    CountryId = 1
+                },
+                new Province
+                {
+                    ProvinceId = 9,
+                    ProvinceName = "ON",
+                    CountryId = 1
+                },
+                new Province
+                {
+                    ProvinceId = 10,
+                    ProvinceName = "PE",
+                    CountryId = 1
+                },
+                new Province
+                {
+                    ProvinceId = 11,
+                    ProvinceName = "QC",
+                    CountryId = 1
+                },
+                new Province
+                {
+                    ProvinceId = 12,
+                    ProvinceName = "SK",
+                    CountryId = 1
+                },
+                new Province
+                {
+                    ProvinceId = 13,
+                    ProvinceName = "YT",
+                    CountryId = 1
+                }
+            );
         }
     }
 }
