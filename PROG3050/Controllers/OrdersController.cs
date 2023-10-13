0b6283aa905befeb146342a6863981968f0b1ece
junseo-yang <jsy724724@gmail.com>
2023-10-13 15:59:39 -0400

    Fix: Scaffold Orders

---
+++ PROG3050/Controllers/OrdersController.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PROG3050.Data;
+using PROG3050.Models;
+
+namespace PROG3050.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrdersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Orders
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Order.Include(o => o.ShippingAddress).Include(o => o.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Orders/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Order == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Order
+                .Include(o => o.ShippingAddress)
+                .Include(o => o.User)
+                .FirstOrDefaultAsync(m => m.OrderId == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // GET: Orders/Create
+        [Authorize(Roles = "SuperAdmin,Admin,Moderator")]
+        public IActionResult Create()
+        {
+            ViewData["ShippingAddressId"] = new SelectList(_context.ShippingAddress, "ShippingAddressId", "City");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Orders/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "SuperAdmin,Admin,Moderator")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("OrderId,UserId,Status,OrderDate,ShippingAddressId")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(order);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ShippingAddressId"] = new SelectList(_context.ShippingAddress, "ShippingAddressId", "City", order.ShippingAddressId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", order.UserId);
+            return View(order);
+        }
+
+        // GET: Orders/Edit/5
+        [Authorize(Roles = "SuperAdmin,Admin,Moderator")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Order == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Order.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            ViewData["ShippingAddressId"] = new SelectList(_context.ShippingAddress, "ShippingAddressId", "City", order.ShippingAddressId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", order.UserId);
+            return View(order);
+        }
+
+        // POST: Orders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "SuperAdmin,Admin,Moderator")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("OrderId,UserId,Status,OrderDate,ShippingAddressId")] Order order)
+        {
+            if (id != order.OrderId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(order);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderExists(order.OrderId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ShippingAddressId"] = new SelectList(_context.ShippingAddress, "ShippingAddressId", "City", order.ShippingAddressId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", order.UserId);
+            return View(order);
+        }
+
+        // GET: Orders/Delete/5
+        [Authorize(Roles = "SuperAdmin,Admin,Moderator")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Order == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Order
+                .Include(o => o.ShippingAddress)
+                .Include(o => o.User)
+                .FirstOrDefaultAsync(m => m.OrderId == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // POST: Orders/Delete/5
+        [Authorize(Roles = "SuperAdmin,Admin,Moderator")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Order == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Order'  is null.");
+            }
+            var order = await _context.Order.FindAsync(id);
+            if (order != null)
+            {
+                _context.Order.Remove(order);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrderExists(int id)
+        {
+          return (_context.Order?.Any(e => e.OrderId == id)).GetValueOrDefault();
+        }
+    }
+}
