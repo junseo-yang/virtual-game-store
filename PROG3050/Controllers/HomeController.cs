f5066a2fe621e9b1685405dab4a24746b6439e9d
samb-park <sangbong.park@outlook.com>
2023-11-05 11:48:51 -0500

    Implemented selecting games feature. - Implemented a search by game titler in the Index (Home). - Implemented the ability to select from the game list in the search results. - When selecting a game, it redirects to the detail page and shows detailed information.

--- PROG3050/Controllers/HomeController.cs
+++ PROG3050/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PROG3050.Data;
 using PROG3050.Models;
+using PROG3050.ViewModel;
 using System.Diagnostics;
 
 namespace PROG3050.Controllers
@@ -7,15 +11,73 @@ namespace PROG3050.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
-
-        public IActionResult Index()
+        
+        public async Task<IActionResult> Index()
         {
             return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Search(HomeViewModel vmFromBody)
+        {
+            HomeViewModel homeViewModel = new HomeViewModel();
+
+            try
+            {
+                if (vmFromBody != null && string.IsNullOrEmpty(vmFromBody.Title))
+                    throw new Exception("PLEASE ENTER A GAME TITLE");
+
+                var games = await _context.Game.Include(g => g.GameCategory).ToListAsync();
+                var title = vmFromBody.Title;
+                var sortedGames = games.Where(x => x.Title.ToLower().Contains(title.ToLower()));
+
+                if (!sortedGames.Any())
+                    throw new Exception("NOT FOUND GAMES");
+
+                homeViewModel.Games = sortedGames;
+
+            }
+            catch(Exception ex)
+            {
+                homeViewModel.ErrorMessage = ex.Message;
+            }
+
+            return View("Index", homeViewModel);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> AllSearch()
+        {
+            var games = await _context.Game.Include(g => g.GameCategory).OrderBy(x => x.GameId).ToListAsync();
+
+            HomeViewModel vm = new HomeViewModel()
+            {
+                Games = games,
+            };
+
+            return View("Index", vm);
+        }
+
+        [Authorize]
+        public async Task<ActionResult> Detail(int id)
+        {
+            var games = await _context.Game.Include(g => g.GameCategory).ToListAsync();
+            var game = games.FirstOrDefault(x => x.GameId.Equals(id));
+            HomeViewModel homVM = new HomeViewModel()
+            {
+                Games = games,
+                CurrentGame = game,
+            };
+
+            return View(homVM);
         }
 
         public IActionResult Privacy()
---
+++ PROG3050/ViewModel/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using PROG3050.Models;
+
+namespace PROG3050.ViewModel
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Game> Games { get; set; }
+
+        public Game? CurrentGame { get; set; }
+
+        public string? Title { get; set; }
+
+        public string ErrorMessage { get; set; } = string.Empty;
+    }
+}
