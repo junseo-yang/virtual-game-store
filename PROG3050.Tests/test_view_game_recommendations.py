5725fc0097b145ab0d022673ed48fd0e1fb48b59
brenbi <brendandonnachie99@gmail.com>
2023-11-08 13:43:13 -0500

    Feature: Game Recommendations and tests (#14)

---
+++ PROG3050.Tests/test_view_game_recommendations.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import chromedriver_autoinstaller
+
+
+chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+                                      # and if it doesn't exist, download it automatically,
+                                      # then add chromedriver to path
+
+"""
+Arrange
+"""
+# Tester1 navigates to the homepage
+driver = webdriver.Chrome()
+driver.get("https://localhost:7239/")
+
+# Tester1 login
+driver.find_element(By.LINK_TEXT, "Login").click()
+driver.find_element(By.ID, "Input_Email").send_keys("Tester1")
+driver.find_element(By.ID, "Input_Password").send_keys("Tester1!@")
+driver.find_element(By.ID, "login-submit").click()
+
+"""
+Act
+"""
+# Tester1 navigates to the games page
+driver.get("https://localhost:7239/Games")
+
+# Tester1 clicks a game
+driver.find_element(By.XPATH, "/html/body/div/main/div/div/table/tbody/tr[1]/td").click()
+
+# Get Recommended Games sub title
+element = driver.find_element(By.XPATH, "/html/body/div/main/div/h4")
+
+"""
+Assert
+"""
+# Assert element text is Recommended Games
+assert element.text == "Recommended Games"
+
+"""
+Clean up
+"""
+# Logout
+driver.find_element(By.ID, "dropdownMenuButton1").click()
+driver.find_element(By.CSS, ".btn-link").click()
+
+driver.quit()
